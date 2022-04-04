d38f4d9be07a8697aa0d4b4c703d675a0535e79e
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-04 17:23:16 +0900

    스크롤에 따라 svg의 strokeDashoffset 값을 변경하여 그려낸다

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const content = document.querySelector(".content1");
-const path = document.querySelector(".path");
+const path = document.querySelector(".path2");
+const pathLength = path.getTotalLength();
+
+path.style.strokeDasharray = pathLength;
+path.style.strokeDashoffset = pathLength;
+
+const calcDashoffset = (scrollY, element, length) => {
+  const ratio = (scrollY - element.offsetTop) / element.offsetHeight;
+  const value = length - length * ratio;
+  // console.log(value);
+  return value < 0 ? 0 : value > length ? length : value;
+};
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY + window.innerHeight;
+  path.style.strokeDashoffset = calcDashoffset(scrollY, content, pathLength);
+});
