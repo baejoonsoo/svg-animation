43dca3d8fa3d3a9bba3c70f85cc95c7b6b10f11c
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-04 18:28:33 +0900

    추가한 svg 두개도 스크롤에 따라 드로우

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const content = document.querySelector(".content1");
-const path = document.querySelector(".path1");
-const pathLength = path.getTotalLength();
+const content1 = document.querySelector(".content1");
+const content2 = document.querySelector(".content2");
+const content3 = document.querySelector(".content3");
+
+const path1 = document.querySelector(".path1");
+const path2 = document.querySelector(".path2");
+const path3 = document.querySelector(".path3");
+
+const pathLength1 = path1.getTotalLength();
+const pathLength2 = path2.getTotalLength();
+const pathLength3 = path3.getTotalLength();
 
 const calcDashoffset = (scrollY, element, length) => {
   const ratio = (scrollY - element.offsetTop) / element.offsetHeight;
@@ -9,14 +17,22 @@ const calcDashoffset = (scrollY, element, length) => {
   return value < 0 ? 0 : value > length ? length : value;
 };
 
-path.style.strokeDasharray = pathLength;
-path.style.strokeDashoffset = calcDashoffset(
+path1.style.strokeDasharray = pathLength1;
+path1.style.strokeDashoffset = calcDashoffset(
   window.offsetHeight * 0.8,
-  content,
-  pathLength
+  content1,
+  pathLength1
 );
+
+path2.style.strokeDasharray = pathLength2;
+path2.style.strokeDashoffset = pathLength2;
+
+path3.style.strokeDasharray = pathLength3;
+path3.style.strokeDashoffset = pathLength3;
 
 window.addEventListener("scroll", () => {
   const scrollY = window.scrollY + window.innerHeight * 0.8;
-  path.style.strokeDashoffset = calcDashoffset(scrollY, content, pathLength);
+  path1.style.strokeDashoffset = calcDashoffset(scrollY, content1, pathLength1);
+  path2.style.strokeDashoffset = calcDashoffset(scrollY, content2, pathLength2);
+  path3.style.strokeDashoffset = calcDashoffset(scrollY, content3, pathLength3);
 });
