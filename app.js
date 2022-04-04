626c09d242ccec98fd69366206170f3998b58067
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-04 18:18:35 +0900

    path class name change

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const content = document.querySelector(".content1");
-const path = document.querySelector(".path2");
+const path = document.querySelector(".path1");
 const pathLength = path.getTotalLength();
-
-path.style.strokeDasharray = pathLength;
-path.style.strokeDashoffset = pathLength;
 
 const calcDashoffset = (scrollY, element, length) => {
   const ratio = (scrollY - element.offsetTop) / element.offsetHeight;
   const value = length - length * ratio;
-  // console.log(value);
+
   return value < 0 ? 0 : value > length ? length : value;
 };
+
+path.style.strokeDasharray = pathLength;
+path.style.strokeDashoffset = calcDashoffset(
+  window.offsetHeight * 0.8,
+  content,
+  pathLength
+);
 
 window.addEventListener("scroll", () => {
   const scrollY = window.scrollY + window.innerHeight * 0.8;
